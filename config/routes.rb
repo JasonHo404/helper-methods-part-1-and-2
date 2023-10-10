4b824fbfb23777a1452ac606977e8f1d6e7c2eaf
jason ho <43012595+JasonHo404@users.noreply.github.com>
2023-10-10 21:35:29 +0000

    route hlpers

--- config/routes.rb
+++ config/routes.rb
@@ -6,24 +6,24 @@ Rails.application.routes.draw do
 
   # CREATE
   #post("/movies",  :controller => "movies", :action => "create" )
-  post "/movies", to:"movies#create"
+  post "/movies", to:"movies#create", as: :movies
   #get("/movies/new",  :controller => "movies", :action => "new" )
-  get "/movies/new", to:"movies#new"       
+  get "/movies/new", to:"movies#new", as: :new_movie
   # READ
   #get("/movies",  :controller => "movies", :action => "index" )
   get "/movies", to:"movies#index"
   #get("  /movies/:id",  :controller => "movies", :action => "show" )
-  get "/movies/:id", to:"movies#show"
+  get "/movies/:id", to:"movies#show", as: :movie
   # UPDATE
  # patch("/movies/:id",  :controller => "movies", :action => "update" )
   patch "/movies/:id", to: "movies#update"
   #get("/movies/:id/edit",  :controller => "movies", :action => "edit" )
-  get "/movies/:id/edit", to: "movies#edit"
+  get "/movies/:id/edit", to: "movies#edit", as: :edit_movie
 
   
   # DELETE
   #delete("/movies/:id",  :controller => "movies", :action => "destroy" )
-  delete "/movies/:id", to: "movies#destroy"
+  delete "/movies/:id", to: "movies#destroy" 
 
   #------------------------------
 end
